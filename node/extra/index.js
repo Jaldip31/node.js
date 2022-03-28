8d7e6f1a6b4fe776d803213501c26360345183e5
jaldipbhalani <jaldipbhalani@gmail.com>
2022-03-28 22:31:21 +0530

    28-03-2022

---
+++ node/extra/index.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+
+// fs.mkdirSync("thapa");                                           //Create folder 
+fs.writeFileSync("thapa/bio.txt","My name is Jaldip....");          //create file
+// fs.appendFile("thapa/bio.txt","I am a good boy")
