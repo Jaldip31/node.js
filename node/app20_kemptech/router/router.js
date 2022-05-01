3ea876a00c44036a8a9d528d2556bd38707c6453
jaldipbhalani <jaldipbhalani@gmail.com>
2022-05-01 18:07:34 +0530

    1-5-22

--- node/app20_kemptech/router/router.js
+++ node/app20_kemptech/router/router.js
@@ -5,40 +5,45 @@ const userModel = require("../model/model");
 const controller = require("../controller/controller");
 
 // mongoose connect
-mongoose.connect("mongodb://localhost:27017/userDB",{
+mongoose.connect("mongodb://localhost:27017/userDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
 const register = express.Router();
-register.post("/signup",controller.signup);
-register.post("/login",controller.login);
-register.put("/update",controller.update);
-register.delete("/delete",controller.delete);
+register.post("/signup", controller.signup);
+register.post("/login", controller.login);
+register.put("/update", controller.update);
+register.delete("/delete", controller.delete);
 
 module.exports = register;
 
 
 // [
 //     {
-//     "email":"jaldip@gmail.com",
-//     "password":"jaldip123"
+//         "email": "jaldip@gmail.com",
+//         "password": "jaldip123",
+//         "salary.total_salary": 10000
 //     },
 //     {
-//     "email":"jatin@gmail.com",
-//     "password":"jatin123"
+//         "email": "jatin@gmail.com",
+//         "password": "jatin123",
+//         "salary.total_salary": 20000
 //     },
 //     {
-//     "email":"rakesh@gmail.com",
-//     "password":"rakesh123"
+//         "email": "rakesh@gmail.com",
+//         "password": "rakesh123",
+//         "salary.total_salary": 30000
 //     },
 //     {
-//     "email":"jay@gmail.com",
-//     "password":"jay123"
+//         "email": "jay@gmail.com",
+//         "password": "jay123",
+//         "salary.total_salary": 40000
 //     },
 //     {
-//     "email":"jayesh@gmail.com",
-//     "password":"jayesh123"
+//         "email": "jayesh@gmail.com",
+//         "password": "jayesh123",
+//         "salary.total_salary": 50000
 //     }
-    
+
 // ]
