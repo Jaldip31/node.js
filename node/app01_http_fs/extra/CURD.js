2a574866a579f1a18a503af445042db0e8c57d44
jaldipbhalani <jaldipbhalani@gmail.com>
2022-05-05 13:19:52 +0530

    5-5-22

--- node/app01_http_fs/extra/CURD.js
+++ node/app01_http_fs/extra/CURD.js
@@ -1,17 +1,17 @@
 const fs = require("fs");
 
-// fs.mkdirSync("thapa");                                               // Create folder
+fs.mkdirSync("thapa");                                               // Create folder
 
-// fs.writeFileSync("thapa/bio.txt","My name is Jaldip....");           // create file
+fs.writeFileSync("thapa/bio.txt","My name is Jaldip....");           // create file
 
-// fs.appendFile("thapa/bio.txt"," I am a good boy.",()=>{})            // add more data
+fs.appendFile("thapa/bio.txt"," I am a good boy.",()=>{})            // add more data
 
-// const data = fs.readFileSync("thapa/bio.txt","utf-8")                // read file
-// console.log(data)
+const data = fs.readFileSync("thapa/bio.txt","utf-8")                // read file
+console.log(data)
 
-// fs.renameSync("thapa/bio.txt","thapa/myBio.txt")                     // rename file
+fs.renameSync("thapa/bio.txt","thapa/myBio.txt")                     // rename file
 
-// fs.unlinkSync("thapa/bio.txt");                                      // delete file
+fs.unlinkSync("thapa/bio.txt");                                      // delete file
 
-// fs.rmdirSync("thapa");                                              // delete folder
+fs.rmdirSync("thapa");                                              // delete folder
 
