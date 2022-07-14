a48727fffb79ca510aa0a2a5ee359c60054cde69
jaldip bhalani <92107664+Jaldip31@users.noreply.github.com>
2022-07-14 10:08:15 +0530

    14/07/2022

--- node/app23_node_mailer(sendgrid)/index.js
+++ node/app23_node_mailer(sendgrid)/index.js
@@ -1,1 +1,43 @@
+const express = require("express");
+const app = express();
+const nodemailer = require("nodemailer")
+const sendgridtransport = require("nodemailer-sendgrid-transport")
 
+
+const transporter = nodemailer.createTransport(sendgridtransport({
+    auth:{
+        api_key:"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" 
+    }
+
+}))
+app.use(express.json());
+//app.use(cors());
+
+app.get("/test", (req, res) => {
+    res.send("hello...")
+    console.log("hiii")
+
+    var a =""
+    for(var i=0;i<1;i++){
+        
+           a += transporter.sendMail({
+            to:"mahesh.micrasolution@gmail.com",
+            from:"lipsajj@gmail.com",
+            subject:"CodeCookies InfoTech",
+            // html:"<h1>kaam karo vatu nai</h1>",
+            html:`<h1>Job Requirements</h1><br>
+<h3>Applying for : web Developer(React/Angular)</h3>
+<h3>Salary : 30000/month</h3>
+<h3>experiences : 0-1 year</h3>
+For more details <a href="https://tenor.com/view/majak-mazak-kapil-sharma-gulati-mashur-gif-18364167">click here</a>`,
+            
+        })
+    }
+    console.log("end")
+    return a
+})
+
+//assign the port number
+app.listen(5252, () => {    
+    console.log("listing port is 5252");
+});
