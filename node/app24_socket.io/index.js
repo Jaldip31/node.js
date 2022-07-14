bac946106fa2d81bda891f14ed322b356642c8b2
jaldip bhalani <92107664+Jaldip31@users.noreply.github.com>
2022-07-14 10:09:09 +0530

    14/07/200

---
+++ node/app24_socket.io/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express();
+const server = require("http").createServer(app)
+const io = require("socket.io")(server,{cors:{origin:"*"}})
+
+app.set("view engine","ejs")
+
+app.get("/home",(req,res)=>{
+    res.render("home")
+})
+
+app.listen(5151,()=>{
+    console.log('server running...');
+})
+
+io.on('connection',(socket)=>{
+    console.log('User connected : ',socket.id);
+})
